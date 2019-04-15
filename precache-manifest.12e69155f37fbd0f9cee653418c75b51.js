eb9181f9565525044e079fe7ae468bc48bd5508a
Nguyễn Đức Mạnh <manh16nd@gmail.com>
2019-04-15 14:07:03 +0700

    Updates

---
+++ precache-manifest.12e69155f37fbd0f9cee653418c75b51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8153523496ed2374ca70",
+    "url": "/static/css/main.add0e69b.chunk.css"
+  },
+  {
+    "revision": "8153523496ed2374ca70",
+    "url": "/static/js/main.d89f2265.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b2c191c6bfeaffb1cea",
+    "url": "/static/js/2.92966a01.chunk.js"
+  },
+  {
+    "revision": "7b24ffced441370dfd4f29d443f985ea",
+    "url": "/index.html"
+  }
+];
