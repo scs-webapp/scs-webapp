0c13d8d503353aa0b22e303d0cf9dbbbdac2e9cc
Nguyễn Đức Mạnh <manh16nd@gmail.com>
2019-04-15 14:04:28 +0700

    changed

--- src/layouts/admin/Admin.js
+++ src/layouts/admin/Admin.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import AppContext from '../../AppContext'
 import * as firebase from 'firebase'
 import AppBar from '@material-ui/core/AppBar'
@@ -8,7 +8,26 @@ import Button from '@material-ui/core/Button'
 import NewSchool from './NewSchool'
 import ListSupporters from './ListSupporters'
 import Messages from './Messages'
+import TextField from '@material-ui/core/TextField'
 import EditSchool from './EditSchool'
+
+function xoa_dau(str) {
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+    str = str.replace(/đ/g, "d");
+    str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+    str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+    str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+    str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+    str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+    str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+    str = str.replace(/Đ/g, "D");
+    return str;
+}
 
 class Admin extends Component {
     constructor(props) {
@@ -19,12 +38,13 @@ class Admin extends Component {
         this.schoolRef.on('child_changed', this.updateSchool)
         this.state = {
             school: {},
-            addNewSchool: false
+            addNewSchool: false,
+            search: '',
         }
     }
 
     updateSchool = (snap) => {
-        this.setState(({school}) => ({
+        this.setState(({ school }) => ({
             school: {
                 ...school,
                 [snap.key]: snap.val(),
@@ -37,13 +57,6 @@ class Admin extends Component {
             selectedSchool: s,
             messages: null,
         })
-        //
-        // firebase.database().ref(this.props.DB_PREFIX- + '/users')
-        //     .orderByChild('group')
-        //     .equalTo(s)
-        //     .once('value').then(snap => {
-        //     var users = this.props.decodeFirebaseArray(snap.val() || {}, 'uid')
-        // })
     }
 
     clickAllMessages = (s) => () => {
@@ -71,7 +84,7 @@ class Admin extends Component {
     }
 
     render() {
-        const {school, selectedSchool, addNewSchool, messages, editSchool} = this.state
+        const { school, selectedSchool, addNewSchool, messages, editSchool } = this.state
 
         return (
             <div className="Admin">
@@ -81,9 +94,23 @@ class Admin extends Component {
                     </Toolbar>
                 </AppBar>
                 <div className={'container mt-3'}>
+
                     <Button variant="contained" color="primary" onClick={this._addNewSchool}>
                         Thêm trường
                     </Button>
+                    <br />
+                    <TextField
+                        id="outlined-name"
+                        label="Tìm kiếm theo tên trường"
+                        margin="normal"
+                        variant='outlined'
+                        fullWidth
+                        value={this.state.search}
+                        onChange={(e) => {
+                            const { value } = e.target
+                            this.setState({ search: value })
+                        }}
+                    />
                     <NewSchool
                         open={addNewSchool}
                         toggle={this._addNewSchool}
@@ -112,50 +139,55 @@ class Admin extends Component {
                         </div>
                         <div className={'card-body'}>
                             <div className="text-muted">Quản lý danh sách tư vấn viên</div>
-                            <ListSupporters school={selectedSchool}/>
+                            <ListSupporters school={selectedSchool} />
                         </div>
                     </div> : messages ? <div className={'card mt-3'}>
-                            <div className={'card-header'}>
+                        <div className={'card-header'}>
 
-                                <div className="d-flex justify-content-between">
-                                    <Button>
-                                        {school[messages].name}
-                                    </Button>
-                                    <Button onClick={() => this.setState({
-                                        messages: null,
-                                    })}>
-                                        Quay lại
+                            <div className="d-flex justify-content-between">
+                                <Button>
+                                    {school[messages].name}
+                                </Button>
+                                <Button onClick={() => this.setState({
+                                    messages: null,
+                                })}>
+                                    Quay lại
                                     </Button>
 
+                            </div>
+                        </div>
+                        <div className={'card-body'}>
+                            <Messages school={messages} />
+                        </div>
+                    </div> : <div className='row'>
+                                {Object.keys(school).filter(s => {
+                                    const sc = school[s]
+                                    return xoa_dau(sc.name.toLowerCase()).includes(xoa_dau(this.state.search.toLocaleLowerCase()))
+                                }).map((s, i) => <div className='col-12 col-md-6 col-lg-6 col-xl-6' key={i}><div className={'card card-body mt-3 Card'}>
+                                    <span
+                                        onClick={this.clickSchool(s)}
+                                        className={'Cursor'}
+                                    >
+                                        {school[s].name}
+                                    </span>
+                                    <Button
+                                        onClick={this.clickAllMessages(s)}
+                                    >
+                                        Xem tin nhắn
+                            </Button>
+                                    <Button
+                                        onClick={this.clickSchool(s)}
+                                    >
+                                        Quản lý tư vấn viên
+                            </Button>
+                                    <Button
+                                        onClick={this.editSchool(s, school[s])}
+                                    >
+                                        Sửa thông tin trường
+                            </Button>
                                 </div>
-                            </div>
-                            <div className={'card-body'}>
-                                <Messages school={messages}/>
-                            </div>
-                        </div> :
-                        Object.keys(school).map((s, i) => <div className={'card card-body mt-3 Card'} key={i}>
-                            <span
-                                onClick={this.clickSchool(s)}
-                                className={'Cursor'}
-                            >
-                                {school[s].name}
-                            </span>
-                            <Button
-                                onClick={this.clickAllMessages(s)}
-                            >
-                                Xem tin nhắn
-                            </Button>
-                            <Button
-                                onClick={this.clickSchool(s)}
-                            >
-                                Quản lý tư vấn viên
-                            </Button>
-                            <Button
-                                onClick={this.editSchool(s, school[s])}
-                            >
-                                Sửa thông tin trường
-                            </Button>
-                        </div>)}
+                                </div>)}
+                            </div>}
                 </div>
             </div>
         )
